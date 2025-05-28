06522f8d682b88c4186675bddd0e8176daab7a5e
burgues0 <victordpp@outlook.com>
2025-05-28 20:23:24 -0300

    add: repo cloning loop

---
+++ apimenu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPOS=("https://github.com/teste1/teste1.git" "https://github.com/teste2/teste2.git")
+
+DOCKERFILES_DIR="./dockerfiles"
+mkdir -p "$DOCKERFILES_DIR"
+
+#clonando os repos
+for repo in "${REPOS[@]}"; do
+    REPO_NAME=$(basename "$repo" .git)
+    if [ ! -d "$REPO_NAME" ]; then
+        git clone "$repo"
+        echo "Repositório $REPO_NAME clonado!"
+    else
+        echo "Repositório $REPO_NAME já existe."
+    fi
+done
