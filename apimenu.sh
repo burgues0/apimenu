f7bbb8275f67cd50660daa3bf7733a74d8e48c37
burgues0 <victordpp@outlook.com>
2025-05-30 20:22:19 -0300

    edit: change dockerfile logic + add repo checks

--- apimenu.sh
+++ apimenu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 REPOS=("https://github.com/levyath/Api_Gerenciamento_de_obras.git")
-
 DOCKERFILES_DIR="./dockerfiles"
+
 mkdir -p "$DOCKERFILES_DIR"
 
 #clonando os repos
@@ -12,21 +12,35 @@ for repo in "${REPOS[@]}"; do
         git clone "$repo"
         echo "Repositório $REPO_NAME clonado!"
     else
-        echo "Repositório $REPO_NAME já existe."
+        echo "Repositório $REPO_NAME já existe. Atualizando repositório a partir da main..."
+        cd "$REPO_NAME" && git pull
     fi
 done
+
+cd ..
 
 # Criando Dockerfiles para cada API
 for repo in "${REPOS[@]}"; do
     REPO_NAME=$(basename "$repo" .git)
     DOCKERFILE_PATH="$DOCKERFILES_DIR/Dockerfile_$REPO_NAME"
 
+    if [ ! -d "$REPO_NAME" ]; then
+        echo "Erro: Diretorio $REPO_NAME não encontrado. Pulando criação do Dockerfile..."
+        continue
+    fi
+
+    if [ ! -f "$REPO_NAME/package.json" ]; then
+        echo "Aviso: package.json não foi encontrado no repo $REPO_NAME."
+    fi
+
     cat > "$DOCKERFILE_PATH" <<EOF
-FROM node:18
+FROM node:18-alpine
 WORKDIR /app
+COPY package*.json ./
+RUN npm ci --only=production
 COPY . .
-RUN npm install
 CMD ["npm", "start"]
+EXPOSE 3000
 EOF
     echo "Dockerfile criado para $REPO_NAME em $DOCKERFILE_PATH"
 done
